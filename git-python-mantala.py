7510a9966a70d791195533a02e6f900033204ea4
MantalaJayRenz <jayrenzmantala25@icloud.com>
2023-03-10 15:52:09 +0800

    My Personal Info added

---
+++ git-python-mantala.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 10 15:49:09 2023
+
+@author: admin
+"""
+
+print("Jay Renz Mantala")
+print("Magapua, Mogpog, Marinduque")
+print("December 15, 2002")
